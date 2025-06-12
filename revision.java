a9d39393ea5a31ddc961c0f6a8994feda1c85764
atanu mandal <atanu2697@gmail.com>
2025-06-13 00:52:36 +0530

    code revised

---
+++ revision.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class revision {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        int n3 = sc.nextInt();
+        int n4 = sc.nextInt();
+        int n5 = sc.nextInt();
+        int n6 = sc.nextInt();
+        int n7 = sc.nextInt();
+        int n8 = sc.nextInt();
+        int n9 = sc.nextInt();
+        int n10 = sc.nextInt();
+
+        System.out.println("no 1 is : " + n1);
+        System.out.println("no 2 is : " + n2);
+        System.out.println("no 3 is : " + n3);
+        System.out.println("no 4 is : " + n4);
+        System.out.println("no 5 is : " + n5);
+        System.out.println("no 6 is : " + n6);
+        System.out.println("no 7 is : " + n7);
+        System.out.println("no 8 is : " + n8);
+        System.out.println("no 9 is : " + n9);
+        System.out.println("no 10 is : " + n10);
+
+        sc.close();
+    }
+}
